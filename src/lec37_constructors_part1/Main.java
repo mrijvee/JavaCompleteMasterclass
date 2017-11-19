dba833fd20ef38bc0478dd5e5cdfae7f6684c496
mrijvee <mezbahrijvee@gmail.com>
2017-11-18 23:41:09 -0800

    constructor update

--- src/lec37_constructors_part1/Main.java
+++ src/lec37_constructors_part1/Main.java
@@ -1,30 +1,14 @@
 package lec37_constructors_part1;
 
-class Parent
-{
-    public Parent()
-    {
-        System.out.println("Parent class no-args constructor called");
-    }
-    public Parent(String name)
-    {
-        System.out.println("Parent class Parameterized constructor called by "+name);
-    }
-}
-public class Child extends Parent
-{
-    public Child()
-    {
-        this("JIP");
-        System.out.println("Child class no-args constructor called");
-    }
-    public Child(String name)
-    {
-        super("JIP");
-        System.out.println("Child class Parameterized constructor called by "+name);
-    }
-    public static void main(String args[])
-    {
-        Child c = new Child();
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account account = new Account();
+        account.withdrawFunds(100);
+
+        account.depositFunds(40);
+        account.withdrawFunds(20);
+        account.depositFunds(10);
     }
 }
